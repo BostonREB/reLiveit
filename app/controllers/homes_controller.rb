580c241eca9e8263dddf6cdcf7fd778c29db9255
Rich Bowman <BostonREB99@gmail.com>
2014-04-28 15:19:57 -0400

    add clearance, bourbon, bitters, & neat

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,13 @@
 class HomesController < ApplicationController
+  before_action :goto_user, if: :signed_in?
 
   def show
 
+  end
+
+private
+
+def goto_user
+    redirect_to user_path(current_user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Relive::Application.routes.draw do
 
   root 'homes#show'
 
+  resources :users, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
