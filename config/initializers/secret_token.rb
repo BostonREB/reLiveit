aff29a2a9be660196a0f06206f3758aad034e54a
Rich Bowman <BostonREB99@gmail.com>
2014-04-28 09:45:49 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Relive::Application.config.secret_key_base = '9da1b9a8d01918a2dc503b3b5280331fb29c5769322ba65fa630d8192f92a41617fa0c28acfa4aafe5bf7c9b82dcf169af9bfb63acc17d451c3c23b9094b3610'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Relive::Application.config.session_store :cookie_store, key: '_relive_session'
