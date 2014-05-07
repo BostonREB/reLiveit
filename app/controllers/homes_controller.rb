d5634e9ed3348ada22f231508290648ac57d577d
Rich Bowman <BostonREB99@gmail.com>
2014-05-07 14:34:09 -0400

    add user notices

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
+  before_action :get_api_data, if: :signed_in?
   before_action :goto_user, if: :signed_in?
 
   def show
@@ -7,23 +8,14 @@ class HomesController < ApplicationController
 
 private
 
+  def get_api_data
+    if current_user.id == 1
+      get_recordings_from_live_music_archive
+      get_tour_dates_from_songkick
+    end
+  end
+
   def goto_user
-    get_recordings_from_lma
-    get_tour_dates_from_songkick
     redirect_to current_user
-  end
-
-  def get_recordings_from_lma
-    current_user.followed_artists.each do |artist|
-      lma_recordings = RecordingRetriever.new(artist)
-      lma_recordings.get_api_data
-    end
-  end
-
-  def get_tour_dates_from_songkick
-    current_user.followed_artists.each do |artist|
-      artist_tour_dates = TourDateRetriever.new(artist)
-      artist_tour_dates.get_tour_api_data
-    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,31 @@
 class UsersController < ApplicationController
 
   def show
-    @followed_artists = current_user.followed_artists
+    @recent_recordings = find_recent_recordings
+    @user_shows = find_all_user_shows
+  end
+
+  private
+
+  def find_recent_recordings
+    recent_recordings = []
+    current_user.followed_artists.each do |artist|
+      artist.recordings.each do |recording|
+        if recording.upload_date.to_date > 21.days.ago
+          recent_recordings << recording
+        end
+      end
+    end
+    recent_recordings
+  end
+
+  def find_all_user_shows
+    user_shows = []
+    current_user.followed_artists.each do |artist|
+      artist.shows.each do |show|
+        user_shows << show
+      end
+    end
+    user_shows
   end
 end
