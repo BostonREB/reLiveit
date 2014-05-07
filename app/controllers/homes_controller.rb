6e08b2b9de6149871bb88fac7a24677f559d8c5d
Rich Bowman <BostonREB99@gmail.com>
2014-05-07 15:17:15 -0400

    fix data generation method

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
+  before_action :get_api_data, if: :signed_in?
   before_action :goto_user, if: :signed_in?
 
   def show
@@ -7,11 +8,14 @@ class HomesController < ApplicationController
 
 private
 
-  def goto_user
+  def get_api_data
     if current_user.id == 1
       get_recordings_from_live_music_archive
       get_tour_dates_from_songkick
     end
+  end
+
+  def goto_user
     redirect_to current_user
   end
 end
