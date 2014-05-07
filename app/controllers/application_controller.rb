392a400c9b0be34ff23290df78a4c3797a30cb95
Rich Bowman <BostonREB99@gmail.com>
2014-05-07 17:24:10 -0400

    add rake tasks to retrieve api data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def get_recordings_from_live_music_archive
-    artists = Artist.all
-    artists.each do |artist|
-      lma_recordings = RecordingRetriever.new(artist)
-      lma_recordings.get_api_data
-    end
-  end
-
-  def get_tour_dates_from_songkick
-    artists = Artist.all
-    artists.each do |artist|
-      artist_tour_dates = TourDateRetriever.new(artist)
-      artist_tour_dates.get_tour_api_data
-    end
-  end
 end
---
+++ lib/tasks/reb.rake
@@ -0,0 +1,18 @@
+namespace :get_data do
+
+  desc "Get bootleg data from Live Music Archive"
+  task :lma => :environment do
+    Artist.find_each do |artist|
+      lma_recordings = RecordingRetriever.new(artist)
+      lma_recordings.get_api_data
+    end
+  end
+
+  desc "Get tour data from Songkick"
+  task :songkick => :environment do
+    Artist.find_each do |artist|
+      artist_tour_dates = TourDateRetriever.new(artist)
+      artist_tour_dates.get_tour_api_data
+    end
+  end
+end
