0661e9fd9ed1aae0d8471985b5c61a8496e24f92
Rich Bowman <BostonREB99@gmail.com>
2014-05-13 14:16:11 -0400

    add browser location shows

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
   before_action :ensure_user_has_location, if: :signed_in?
 
   def show
-
+    @user_location_shows = get_shows_by_location
   end
 
   private
@@ -13,5 +13,12 @@ class HomesController < ApplicationController
     else
       redirect_to current_user
     end
+  end
+
+  def get_shows_by_location
+    location = request.remote_ip
+    remote = Songkickr::Remote.new TourDateRetriever::SONGKICK_API_KEY
+    raw_data = remote.events(location: "ip:#{location}")
+    raw_data.results
   end
 end
