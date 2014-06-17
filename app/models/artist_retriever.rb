aad039df716d147459b81dec1560a34d3f03d992
Rich Bowman <BostonREB99@gmail.com>
2014-06-17 14:20:04 -0400

    change artist retriever

--- app/models/artist_retriever.rb
+++ app/models/artist_retriever.rb
@@ -6,7 +6,7 @@ class ArtistRetriever
   # end
 
   def get_artists
-    raw_data = HTTParty.get("http://archive.org/advancedsearch.php?q=mediatype%3Acollection+AND+collection%3Aetree&fl%5B%5D=identifier&fl%5B%5D=title&sort%5B%5D=titleSorter+asc&sort%5B%5D=&sort%5B%5D=&rows=15000&page=1&indent=yes&output=json")
+    raw_data = HTTParty.get("http://archive.org/advancedsearch.php?q=mediatype%3Acollection+collection%3Aetree&fl%5B%5D=downloads&fl%5B%5D=title&sort%5B%5D=downloads+desc&sort%5B%5D=&sort%5B%5D=&rows=1800&page=1&indent=yes&output=json")
     artists = raw_data['response']['docs']
     save_artists(artists)
   end
