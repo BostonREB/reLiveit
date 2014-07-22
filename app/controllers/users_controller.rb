8b93d757335649ab44a4d6926fded143fbb5763a
Rich Bowman <BostonREB99@gmail.com>
2014-07-22 14:55:38 -0400

    some minor refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
     @recent_recordings = current_user.recordings.recent
     @user_shows = current_user.shows.by_date
     @user_location_shows = get_shows_by_location
-    @followed_artists = get_followed_artists
   end
 
   def update
@@ -18,14 +17,5 @@ class UsersController < ApplicationController
     params.require(:user).permit(
       :gets_email
     )
-  end
-
-  def get_followed_artists
-    followed_acts = []
-    acts = FollowingArtistRelationships.all
-    acts.each do |act|
-      followed_acts << act.followed_artist_id(&:name)
-    end
-    followed_acts
   end
 end
